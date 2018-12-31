02a368a55f756259a6c392739dde11996b159150
hm.park <ajsj9706@naver.com>
2018-12-31 17:47:24 +0900

    자바의 정석 2장 진다문제

--- src/Ch02진단문제/Ch02Ex01_01.java
+++ src/Ch02진단문제/Ch02Ex01_01.java
@@ -3,7 +3,7 @@ package Ch02진단문제;
 public class Ch02Ex01_01 {
 
 	public static void main(String[] args) {
-		System.out.println("Programming! It's fun.");
+		System.out.println("Programming! It's fun..");
 
 	}
 
